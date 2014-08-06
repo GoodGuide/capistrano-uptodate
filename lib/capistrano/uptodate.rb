5f875d790cba213a554682e818e70d63a3889865
Ryan Taylor Long <ryan@rtlong.com>
2014-08-05 22:18:49 -0700

    Simplify and expand specifying upstream and logic to check if different

--- lib/capistrano/uptodate.rb
+++ lib/capistrano/uptodate.rb
@@ -13,40 +13,36 @@ Capistrano::Configuration.instance(true).load do
     task :git do
       scm = fetch(:uptodate_scm, :git)
       scm_binary = fetch(:uptodate_scm_binary, 'git')
-      remote_repository = fetch(:uptodate_remote_repository, 'origin')
-      branch = fetch(:uptodate_branch, 'master')
+      remote_ref = fetch(:uptodate_remote_ref, 'origin/master')
       time = fetch(:uptodate_time, 300)
-      behavior = fetch(:uptodate_behaviour, :confirm)
+      behavior = fetch(:uptodate_behaviour, fetch(:uptodate_behavior, 'confirm'))
       git_dir = fetch(:uptodate_local_repository, `#{scm_binary} rev-parse --git-dir`.strip)
 
       # skip if no git dir detected
       next if git_dir.empty?
 
-      # skip if remote is not yet configured
-      remote_reference_file = File.join(git_dir, 'refs', 'remotes', remote_repository, branch)
-      next unless File.exists?(remote_reference_file)
-
       # fetch remote references and skip on error
       fetch_file = File.join(git_dir, "FETCH_HEAD")
       unless File.exist?(fetch_file) && Time.now - File.mtime(fetch_file) < time
-        Capistrano::CLI.ui.say "Fetching references from #{remote_repository} repository ..."
-        next unless system("#{scm_binary} fetch #{remote_repository}")
+        Capistrano::CLI.ui.say "Fetching remote git repo..."
+        next unless system("#{scm_binary} fetch")
       end
 
-      # get commit for local and remote reference
-      local_commit  = `#{scm_binary} rev-parse #{branch}`.strip
-      remote_commit = `#{scm_binary} rev-parse #{remote_repository}/#{branch}`.strip
+      # compare local tree and remote ref
+      next if system("#{scm_binary} diff --quiet #{remote_ref}")
 
-      # compare local and remote commit
-      if local_commit != remote_commit
-        Capistrano::CLI.ui.say "Local '#{branch}' branch is not synchronized with '#{remote_repository}' repository."
+      # otherwise, they're different
+      remote_ref_parsed = `#{scm_binary} rev-parse --symbolic-full-name #{remote_ref}`.chomp
 
-        case behavior
-        when :confirm
-          Capistrano::CLI.ui.ask("Continue anyway? (y/N)") == 'y' or abort
-        else
-          abort
-        end
+      Capistrano::CLI.ui.say "Local git tree is not synchronized with #{remote_ref_parsed}"
+
+      case behavior
+      when 'confirm'
+        Capistrano::CLI.ui.ask("Continue anyway? (y/N)") == 'y' or abort
+      when 'ignore'
+        next
+      else
+        abort
       end
     end
   end
